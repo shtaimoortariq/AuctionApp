fc504a59841ecee2172b8354b694bd8503b3b377
taimoortariqdev <taimoortariqdev@gmail.com>
2017-09-14 15:43:38 +0500

    basic leaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { StoreModule } from "@ngrx/store";
+//import { simpleReducer } from "./simple.reducer";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { postReducer } from './reducers/post.reducer';
 import { AppComponent } from './app.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot(
+    { 
+      post: postReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10 // number of states to retain
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
